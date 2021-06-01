2bdcbc2b463932dcfe75e809e9e1b475774079da
ji1kang <jiwonkang@skku.edu>
2021-06-01 23:33:25 +0900

    [feat] update for scripts

--- graphql/db.ts
+++ graphql/db.ts
@@ -16,10 +16,31 @@ export const getCharacter = async () => {
   return rows;
 };
 
+export const getChapter = async () => {
+  const [rows] = await pool.query("select * from series");
+  return rows;
+};
+
+export const getScript = async ({series}) => {
+  const [rows] = await pool.query(
+    "select * from scripts where `series`=? order by line", [series]);
+  return rows;
+};
+
 export const newVoice = async ({ id, voice }) => {
   const result = await pool.execute(
     "update characters SET `voice`= ? where `id` = ?",
     [voice, id])
-  const [rows] = await pool.query("select * from characters where `id`=?", [id]);
+  const [rows] = await pool.query(
+    "select * from characters where `id`=?", [id]);
+  return rows[0];
+}
+
+export const updateSpeaker = async ({ series, line, speaker }) => {
+  const result = await pool.execute(
+    "update scripts SET `speaker`= ? where `series` = ? and `line` = ?",
+    [speaker, series, line])
+  const [rows] = await pool.query(
+    "select * from scripts where `series` = ? and `line` = ?", [series, line]);
   return rows[0];
 }
--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,11 +1,14 @@
-import { getCharacter, newVoice } from "./db";
+import { getCharacter, newVoice, getChapter, getScript, updateSpeaker } from "./db";
 
 const resolvers = {
   Query: {
     characters: () => getCharacter(),
+    chapters: () => getChapter(),
+    scripts: (_, { series }) => getScript({ series }),
   },
   Mutation: {
     updateVoice: (_, { id, voice }) => newVoice({ id, voice }),
+    updateSpeaker: (_, { series, line, speaker }) => updateSpeaker({ series, line, speaker }),
   }
 };
 
